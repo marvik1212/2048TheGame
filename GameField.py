cd673df3b90ab6a6832cefd007b00c9f27a246e1
Ivan <vanyatikhonov20052005@gmail.com>
2024-06-13 20:41:27 +0400

    added main logic function (GameField/movefunc) and UP move (also there)

--- GameField.py
+++ GameField.py
@@ -3,7 +3,7 @@ import random
 class GameField:
     field: list
     gameFlag: bool
-    bigNums: int
+    bigNumsCounter: int
     choiceList: list
     choiceListTwo: list
 
@@ -12,15 +12,15 @@ class GameField:
         for i in range(16):
             temp.append(0)
             '''
-            temp = [[], [], [], []  0 1 2 3
-                    [], [], [], []  4 5 6 7
-                    [], [], [], []  8 9 10 11
-                    [], [], [], []] 12 13 14 15
+            temp = [0, 0, 0, 0   0 1 2 3
+                    0, 0, 0, 0   4 5 6 7
+                    0, 0, 0, 0   8 9 10 11
+                    0, 0, 0, 0]  12 13 14 15
             '''
         self.field = temp
-        self.choiceList = [21, 412]
+        self.choiceList = [2, 4]
         self.choiceListTwo = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-        self.bigNums = 0
+        self.bigNumsCounter = 0
         self.gameFlag = True
 
 
@@ -33,10 +33,10 @@ class GameField:
         return True if counter == 16 else False
 
     def getBigNumsOnBoard(self) -> int:
-        return self.bigNums
+        return self.bigNumsCounter
 
     def incrementBigNumsOnBoard(self) -> None:
-        self.bigNums += 1
+        self.bigNumsCounter += 1
 
     def getGameField(self) -> list:
         return self.field
@@ -52,7 +52,7 @@ class GameField:
         self.field[firstplace] = firstnum
         self.field[secondplace] = secondnum
 
-    def printField(self) -> str:
+    def printField(self):
         tempStr = ''
         tempList = [3, 7, 11, 15]
         for i in range(16):
@@ -64,6 +64,157 @@ class GameField:
                 print(tempStr.ljust(5))
                 tempStr = ''
         print()
+
+    def moveFunc(self, a, b, c, d): # 0 = f[a], 1 = f[b], 2 = f[c], 3 = f[d], ГДЕ a - клетка притяжения
+        f = self.getGameField()
+        # Логика функции - линейная обработка всех кейсов. Сначала рассматриваются случаи, когда есть что складывать
+        while True:
+            if f[a] == f[b] and f[a] != 0 and f[b] != 0:  #1
+                f[a] = f[b] * 2
+                f[b] = 0
+                if f[c] == f[d] and f[c] != 0 and f[d] != 0:
+                    f[b] = f[c] * 2
+                    f[c] = 0
+                    f[d] = 0
+                    break
+                else:
+                    f[b] = f[c]
+                    f[c] = f[d]
+                    f[d] = 0
+                    break
+
+            if f[a] == f[c] and f[a] != 0 and f[c] != 0: #2
+                if f[b] == 0:
+                    f[a] = f[c] * 2
+                    f[b] = f[d]
+                    f[c] = 0
+                    f[d] = 0
+                    break
+
+            if f[a] == f[d] and f[a] != 0 and f[d] != 0: #3
+                if f[b] == 0 and f[c] == 0:
+                    f[a] = f[d] * 2
+                    f[d] = 0
+                    break
+
+            if f[b] == f[c] and f[b] != 0 and f[c] != 0: #4
+                if f[a] != 0:
+                    f[b] = f[c] * 2
+                    f[c] = f[d]
+                    f[d] = 0
+                    break
+                else:
+                    f[a] = f[c] * 2
+                    f[b] = f[d]
+                    f[d] = 0
+                    break
+
+            if f[b] == f[d] and f[b] != 0 and f[d] != 0: #5
+                if f[c] == 0:
+                    if f[a] != 0:
+                        f[b] = f[d] * 2
+                        f[d] = 0
+                        break
+                    else:
+                        f[a] = f[d] * 2
+                        f[d] = 0
+                        break
+
+            if f[c] == f[d] and f[c] != 0 and f[d] != 0: #6
+                if f[a] != 0 and f[b] != 0:
+                    f[c] = f[d] * 2
+                    f[d] = 0
+                    break
+                if f[a] != 0 and f[b] == 0:
+                    f[b] = f[d] * 2
+                    f[d] = 0
+                    f[b] = 0
+                    break
+                if f[a] == 0 and f[b] != 0:
+                    f[a] = f[b]
+                    f[b] = f[d] * 2
+                    f[c] = 0
+                    f[d] = 0
+                    break
+                if f[a] == 0 and f[b] == 0:
+                    f[a] = f[d] * 2
+                    f[c] = 0
+                    f[d] = 0
+                    break
+                # Далее идут кейсы, когда складывать нечего и нужно просто сделать сдвиг
+            else:
+                # One zero (amount of bitches rn)
+                if f[a] == 0 and f[b] != 0 and f[c] != 0 and f[d] != 0: #1
+                    f[a] = f[b]
+                    f[b] = f[c]
+                    f[c] = f[d]
+                    f[d] = 0
+                    break
+                if f[a] != 0 and f[b] == 0 and f[c] != 0 and f[d] != 0: #2
+                    f[b] = f[c]
+                    f[c] = f[d]
+                    f[d] = 0
+                    break
+                if f[a] != 0 and f[b] != 0 and f[c] == 0 and f[d] != 0: #3
+                    f[c] = f[d]
+                    f[d] = 0
+                    break
+                if f[a] != 0 and f[b] != 0 and f[c] != 0 and f[d] != 0: #4
+                    break
+                # Two zeros
+                if f[a] == 0 and f[b] == 0 and f[c] != 0 and f[d] != 0: #5
+                    f[a] = f[c]
+                    f[b] = f[d]
+                    f[c] = 0
+                    f[d] = 0
+                    break
+                if f[a] == 0 and f[b] != 0 and f[c] != 0 and f[d] == 0: #6
+                    f[a] = f[b]
+                    f[b] = f[c]
+                    f[c] = 0
+                    break
+                if f[a] != 0 and f[b] != 0 and f[c] == 0 and f[d] == 0: #7
+                    break
+                if f[a] != 0 and f[b] == 0 and f[c] != 0 and f[d] == 0: #8
+                    f[b] = f[c]
+                    f[c] = 0
+                    break
+                if f[a] == 0 and f[b] != 0 and f[c] == 0 and f[d] != 0: #9
+                    f[a] = f[b]
+                    f[b] = f[d]
+                    f[d] = 0
+                    break
+                if f[a] != 0 and f[b] == 0 and f[c] == 0 and f[d] != 0: #10
+                    f[b] = f[d]
+                    f[d] = 0
+                    break
+                # Three zeros
+                if f[a] == 0 and f[b] == 0 and f[c] == 0 and f[d] != 0: #11
+                    f[a] = f[d]
+                    f[d] = 0
+                    break
+                if f[a] == 0 and f[b] == 0 and f[c] != 0 and f[d] == 0: #12
+                    f[a] = f[c]
+                    f[c] = 0
+                    break
+                if f[a] == 0 and f[b] != 0 and f[c] == 0 and f[d] == 0: #13
+                    f[a] = f[b]
+                    f[b] = 0
+                    break
+                if f[a] != 0 and f[b] == 0 and f[c] == 0 and f[d] == 0: #14
+                    break
+                # Four zeros
+                elif f[a] == 0 and f[b] == 0 and f[c] == 0 and f[d] == 0: #15
+                    break
+                break
+
+    def up(self):
+        self.moveFunc(0, 4, 8, 12)
+        self.moveFunc(1, 5, 9, 13)
+        self.moveFunc(2, 6, 10, 14)
+        self.moveFunc(3, 7, 11, 15)
+
+
 
 
 
--- testing.py
+++ testing.py
@@ -1,12 +1,29 @@
 from GameField import GameField
 from pyfiglet import figlet_format
 g = GameField()
-g.initField()
-s = ''
-for i in g.field:
-    s += str(i)
-print(figlet_format("[  4  ]     [  2  ]     [  128  ]", font="big"))
-input()
+#g.initField()
+g.field[0] = 0
+g.field[4] = 0
+g.field[8] = 2
+g.field[12] = 2
+
+g.field[1] = 4
+g.field[5] = 2
+g.field[9] = 2
+g.field[13] = 4
+
+g.field[2] = 2
+g.field[6] = 4
+g.field[10] = 2
+g.field[14] = 4
+g.printField()
+g.up()
+print()
+print()
+g.printField()
+g.up()
+g.printField()
+
 
 
 
